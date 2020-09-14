6bb0c6cd121978698711a47e273fc2f442298901
Oziel Perez <OZLperez11@gmail.com>
2020-09-14 12:26:29 -0500

    Added functions in bloc to open database and persist contacts

--- lib/blocs/ContactsListBloc.dart
+++ lib/blocs/ContactsListBloc.dart
@@ -1,12 +1,44 @@
 import 'dart:async';
-
-import 'package:call_me/blocs/Bloc.dart';
 import 'package:call_me/models/Contact.dart';
+import 'package:call_me/blocs/Bloc.dart';
+import 'package:call_me/database/ContactDAO.dart';
+import 'package:call_me/database/SQLiteQueryExecutor.dart';
 
 class ContactsListBloc implements Bloc 
 {
+    final SQLiteQueryExecutor _dao;
     final _controller = StreamController<List<Contact>>();
+    List<Contact> _contactList = [];
+
     Stream<List<Contact>> get contactListStream => _controller.stream;
-    void setContactList(List<Contact> contactList) => _controller.sink.add(contactList);
-    void dispose() => _controller.close();
+
+    ContactsListBloc({SQLiteQueryExecutor dao}): _dao = dao ?? ContactDAO();
+
+    Future<ContactsListBloc> init() async {
+        await _dao.open();
+        await setContactList(_contactList);
+    }
+
+    Future<void> setContactList(List<Contact> contactList) async {
+        _contactList = await _dao.selectAll();
+        _controller.sink.add(_contactList);
+    }
+
+    Future<void> addContact(Contact contact) async {
+        contact.id = await _dao.insert(contact);
+        _contactList.add(contact);
+        _contactList.sort(Contact.compare);
+        _controller.sink.add(_contactList);
+    }
+
+    Future<void> removeContact(int id) async {
+        await _dao.delete(id);
+        _contactList.removeWhere((contact) => contact.id == id);
+        _controller.sink.add(_contactList);
+    }
+
+    void dispose() {
+        _dao.close();
+        _controller.close();
+    }
 }
